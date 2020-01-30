Correct gradient log likelihood
Correct mistake in the gradient of the log likelihood, that needs to take into account the gradient of the determinant of the Jacobian. Add a method that compute the gradient of the log of the determinant of the jacobian. Add a few comments.
Add monitoring convergence using mean computation in post_process

pybitup/bayesian_inference.py
@@ -43,6 +43,10 @@ class BayesianPosterior(pybitup.distributions.ProbabilityDistribution):
         return bayes_post 
 
     def compute_log_value(self, Y): 
+        """ Compute the value of the logarithm of the distribution. 
+        In the Bayesian framework, this is the posterior distribution, which takes into accout 
+        the likelihood, the determinant of the jacobian (if there is a change of variable) and the prior.
+        The log(det(jac)) is by default equal to zero if there is no change of variable. """ 
 
         X = self.model.parametrization_backward(Y) 
 
@@ -70,15 +74,21 @@ class BayesianPosterior(pybitup.distributions.ProbabilityDistribution):
     #     return log_like_val
 
     def compute_grad_log_value(self, Y): 
+        """ Compute the gradient of the logarithm of the distribution. 
+        In the Bayesian framework, this is the log of the posterior distribution, which takes into 
+        account the likelihood, the determinant of the jacobian and the prior (!!!! prior still needs to be added here, 
+        only constant prior works so far). """
 
         X = self.model.parametrization_backward(Y)
 
         grad_log_like = self.likelihood.compute_grad_log_value(X)
+        grad_log_det_jac = self.likelihood.compute_grad_log_det_jac(X)
 
-        return grad_log_like
+        return grad_log_like - grad_log_det_jac 
+
 
     def estimate_hessian_model(self, Y): 
-        """ Estimate the Hessian of the model using its analytical gradeints. """ 
+        """ Estimate the Hessian of the model using its analytical gradients. """ 
 
         X = self.model.parametrization_backward(Y)
 
@@ -446,7 +456,7 @@ class Likelihood:
         self.arg_LL = J
 
 
-    # Implementation of the 15-01-20 
+    # # Implementation of the 15-01-20 
     # def compute_grad_log_value(self, X): 
 
     #     grad = np.zeros(len(X))
@@ -495,14 +505,26 @@ class Likelihood:
                 for j in range(nspecies): 
                     grad_model_i = np.concatenate((grad_model_i, self.models[model_id].model_grad[j, pn]))
                 
+                my_mat = np.matmul(ss_x, grad_model_i)  # ss_x * grad_model_i 
+                #grad = np.dot(inv_jac, my_mat) 
                 for k, pn2 in enumerate(self.models[model_id].unpar_name):
-                    prod_i_k = ss_x * grad_model_i * inv_jac[i, k]
-
-                    grad[k] = grad[k] + np.sum(prod_i_k, axis=0)
+                    #prod_i_k = my_mat * inv_jac[i, k]
+                    grad[k] = grad[k] + my_mat * inv_jac[i, k]
 
         return grad
 
 
+    def compute_grad_log_det_jac(self, X):
+
+        for model_id in self.models.keys(): 
+
+            inv_jac = self.models[model_id].parametrization_inv_jac(X)
+            grad_det_jac = self.models[model_id].grad_det_jac(X)
+            det_jac = self.models[model_id].parametrization_det_jac(X)
+
+            grad_log_det_jac = np.matmul(grad_det_jac, inv_jac)/det_jac
+
+        return grad_log_det_jac
 
     def compute_hess_PSD(self, X): 
 

pybitup/post_process.py
@@ -128,19 +128,19 @@ def post_process_data(input_file_name):
     if (inputFields.get("MarkovChain") is not None) or (inputFields.get("Posterior") is not None) or  (inputFields.get("Propagation") is not None):
 
 
-        reader = pd.read_csv('output/mcmc_chain.csv')
+        reader = pd.read_csv('output/mcmc_chain.csv', header=None)
         param_value_raw = reader.values
-        n_samples = len(param_value_raw[:, 0]) + 1
+        n_samples = len(param_value_raw[:, 0])  # + 1
 
-        # Load the samples of the distribution                        
-        param_value_raw = np.zeros((n_samples, n_unpar))
-        with open('output/mcmc_chain.dat', 'r') as file_param:
-            i = 0
-            for line in file_param:
-                c_chain = line.strip()
-                param_value_raw[i, :] = np.fromstring(
-                    c_chain[1:len(c_chain)-1], sep=' ')
-                i += 1
+        # # Load the samples of the distribution                        
+        # param_value_raw = np.zeros((n_samples, n_unpar))
+        # with open('output/mcmc_chain.dat', 'r') as file_param:
+        #     i = 0
+        #     for line in file_param:
+        #         c_chain = line.strip()
+        #         param_value_raw[i, :] = np.fromstring(
+        #             c_chain[1:len(c_chain)-1], sep=' ')
+        #         i += 1
         # -------------------------------------------
         # --------- Plot markov chains --------------
         # -------------------------------------------
@@ -166,6 +166,15 @@ def post_process_data(input_file_name):
 
                 print("{}: mean value = {}; standard dev. = {}; cv = {}; cqv = {}".format(unpar_name[i], c_mean_val, c_std_val, cv, cqv))
 
+
+                # Computing convergence criteria and graphs for each chains 
+                # From Gelman et al., Bayesian Data Analysis, 2014. 
+                mean_it = np.zeros(n_samples-1)
+                plt.figure(1000+i)
+                for it in  range(n_samples-1): 
+                    mean_it[it] = np.mean(param_value_raw[0:it+1, i])
+
+                plt.plot(range(n_samples-1), mean_it)
 
             """
             cov_c = np.cov(param_value_raw, rowvar=False)
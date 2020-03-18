d8060d11c3f809ffaa4902248cf10133e703be6c
Martin-Lacroix <47146335+Martin-Lacroix@users.noreply.github.com>
2020-03-18 12:20:03 +0100

    Test Case Heat

--- testCases/heat_conduction/run.py
+++ testCases/heat_conduction/run.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../../')
+
 import heat_conduction
 import pybitup
 
@@ -9,6 +12,10 @@ heat_conduction_model[case_name] = heat_conduction.HeatConduction()
 
 post_dist = pybitup.solve_problem.Sampling(input_file_name)
 post_dist.sample(heat_conduction_model)
+post_dist.__del__()
+
+post_dist = pybitup.solve_problem.Propagation(input_file_name)
+post_dist.propagate(heat_conduction_model)
 post_dist.__del__()
 
 pybitup.post_process.post_process_data(input_file_name)

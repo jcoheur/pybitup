e825000aa0570314e5d0e76a35f5edc4a913efba
jcoheur <40399092+jcoheur@users.noreply.github.com>
2020-09-25 16:47:23 +0200

    Create class for binObject
    Create class for monte carlo method with binning in order to merge the three different functions.

--- examples/pyrolysis/run_rescaled.py
+++ examples/pyrolysis/run_rescaled.py
@@ -37,13 +37,15 @@ pyro_model["one_reaction_pyrolysis_rescaled"]  = one_reaction_pyrolysis_rescaled
 # print(X) 
 
 
-post_dist = pybitup.solve_problem.Sampling(input_file_name)
-post_dist.sample(pyro_model)
-post_dist.__del__()
+# post_dist = pybitup.solve_problem.Sampling(input_file_name)
+# post_dist.sample(pyro_model)
+# post_dist.__del__()
 
-post_dist = pybitup.solve_problem.Propagation(input_file_name)
-post_dist.propagate(pyro_model)
-post_dist.__del__()
+# post_dist = pybitup.solve_problem.Propagation(input_file_name)
+# post_dist.propagate(pyro_model)
+# post_dist.__del__()
 
-pybitup.post_process.post_process_data(input_file_name)
+post_dist = pybitup.sensitivity_analysis.SensitivityAnalysis(input_file_name, case_name)
+
+# pybitup.post_process.post_process_data(input_file_name)
 

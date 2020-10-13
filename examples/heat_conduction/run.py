9ffa2b5dc3ea02ae5c9ed7167bdb340f8b27990c
jcoheur <40399092+jcoheur@users.noreply.github.com>
2020-10-13 10:12:14 +0200

    Correct spring model example

--- examples/heat_conduction/run.py
+++ examples/heat_conduction/run.py
@@ -14,8 +14,8 @@ post_dist = pybitup.solve_problem.Sampling(input_file_name)
 post_dist.sample(heat_conduction_model)
 post_dist.__del__()
 
-post_dist = pybitup.solve_problem.Propagation(input_file_name)
-post_dist.propagate(heat_conduction_model)
-post_dist.__del__()
+# post_dist = pybitup.solve_problem.Propagation(input_file_name)
+# post_dist.propagate(heat_conduction_model)
+# post_dist.__del__()
 
 pybitup.post_process.post_process_data(input_file_name)

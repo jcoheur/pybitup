cbf67ef1eaabdbcea71cc5afbb68e59d912ab252
jcoheur <40399092+jcoheur@users.noreply.github.com>
2020-10-16 16:04:07 +0200

    Add tutorial in doc
    Add a tutorial in the documentation file. The tutorial is made on the spring model examples.

--- examples/spring_model/spring_model.py
+++ examples/spring_model/spring_model.py
@@ -1,18 +1,14 @@
 import numpy as np
-import math
-
 from pybitup import bayesian_inference as bi
 
 class SpringModel(bi.Model): 
 	""" Class for the spring model """	
 	
 	def __init__(self, x=[], param=[], name=""): 
-	
 		# Initialize parent object ModelInference
 		bi.Model.__init__(self, name=name)	
 	
 	def set_param_values(self):
-
 		# Parameters 
 		self.C = self.param[0] 
 		self.K = self.param[1]
@@ -21,8 +17,8 @@ class SpringModel(bi.Model):
 		self.time = self.x 
 	
 	def fun_x(self):
-	
 		# Get the parameters
 		self.set_param_values() 
-		
-		return 2 * np.exp(-self.C * self.time / 2) * np.cos(math.sqrt(self.K - self.C**2 / 4) * self.time)
+
+        # Return the function evaluation 		
+		return 2 * np.exp(-self.C * self.time / 2) * np.cos(np.sqrt(self.K - self.C**2 / 4) * self.time)

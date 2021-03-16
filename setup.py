d7550fdd12a0c79c525e95a1ef638ee511ca605e
jcoheur <40399092+jcoheur@users.noreply.github.com>
2021-03-16 10:44:31 +0100

    update setup with versioneer

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import pathlib 
 from setuptools import setup
+import versioneer
 
 path_to_requirement_file = pathlib.Path("requirements.txt")
 with open(path_to_requirement_file) as requirements_file:
@@ -9,12 +10,14 @@ with open(path_to_requirement_file) as requirements_file:
                     
 setup(
     name='pybitup',
-    version='1.0',
+	version=versioneer.get_version(),
+	cmdclass=versioneer.get_cmdclass(),
     packages=['pybitup', 'pybitup.pce'],
     url='https://github.com/jcoheur/pybitup/',
     license='',
     author='Joffrey Coheur',
     author_email='joffrey.coheur@uliege.be',
     description='Python Bayesian Inference Toolbox and Uncertainty Propagation',
+	long_description=readme,
 	install_requires=requirements
 )
